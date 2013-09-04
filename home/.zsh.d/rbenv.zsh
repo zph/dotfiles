0126afe4c2bea2a34d6d1b97a16a88eedfc11238
ZPH <Zander@civet.ws>
2013-09-04 00:16:52 -0400

    rbenv zsh only loads when rbenv installed
    Former-commit-id: 8c630ba8bbb46cb6177235cba55a1e6cf4d80c3f

--- home/.zsh.d/rbenv.zsh
+++ home/.zsh.d/rbenv.zsh
@@ -1,6 +1,8 @@
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-# Abbreviation for "gem install".
-gi() { gem install $@; rbenv rehash; rehash }
+if [[ -x `which rbenv` ]]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+  # Abbreviation for "gem install".
+  gi() { gem install $@; rbenv rehash; rehash }
 
-alias rb=rbenv
+  alias rb=rbenv
+fi
