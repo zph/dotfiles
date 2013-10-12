c58e4b5941fb3c167ef1fffa2643899f015a51c5
Zander Hill <Zander@civet.ws>
2013-10-12 01:57:06 -0400

    Add pivotal-open fun in emacs
    Former-commit-id: 356d7c38553ad167cc7046a14b72318220d8307b

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -1,3 +1,4 @@
+;;; Custom Emacs Config
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -7,7 +8,11 @@
  '(column-number-mode t)
  '(custom-safe-themes (quote ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(erc-stamp-mode t)
+  '(org-datetree-add-timestamp (quote inactive))
+ '(org-default-notes-file "~/Dropbox/org_mode/incoming.org")
+ '(org-directory "~/Dropbox/org_mode/")
  '(org-startup-truncated nil)
+
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil))
@@ -23,7 +28,6 @@
           '(lambda ()
              (server-switch-buffer (other-buffer))))
 (server-start)
-
 (setq frame-title-format "%b")		; use buffer name for title
 (setq display-buffer-reuse-frames t)    ; no new frame if already open
 ;; use this shell alias for emacs to run emacsclient unless emacs not running, then run server
@@ -54,6 +58,64 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 (global-set-key (kbd "M-p") 'shell-command)
+(require 'ob-sml nil 'noerror)
+(global-set-key (kbd "<f12>") 'whitespace-mode)
+;(add-hook 'org-mode-hook (lambda () (
+;  ('whitespace-mode)
+;)))
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
+(define-key global-map (kbd "C-c c") 'org-capture)
 
+(setq org-capture-templates
+      ; '(("Incoming" ?i "** %t: %?\n  %i\n  %a"  "Uncategorized")))
+  '(
+    ("n" "Notes" entry (file+datetree "~/Dropbox/org_mode/incoming.org")
+    "* %^{Description} %^g %?
+    Added: %U")
+    ("t" "Todo" entry (file+headline (concat org-directory "/incoming.org") "Tasks")
+       "* TODO %?\n %i\n")
+      ("l" "Link" plain (file (concat org-directory "/incoming.org"))
+       "- %?\n %x\n")
+  )
+)
+(require 'org-install)
+(require 'ob-tangle)
+(global-set-key (kbd "<f8>") 're-builder) ; helps with building regexes
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;; This will transform ansi color to faces in Emacs shell!
+(ansi-color-for-comint-mode-on)
+(defun eshell-handle-ansi-color ()
+  (ansi-color-apply-on-region eshell-last-output-start
+                              eshell-last-output-end))
+(add-hook 'eshell-mode-hook
+          '(lambda ()
+             (add-to-list
+              'eshell-output-filter-functions
+              'eshell-handle-ansi-color)))
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '( (perl . t)
+    (ruby . t)
+    (sh . t)
+    (python . t)
+    (emacs-lisp . t)
+    ))
+(setq org-confirm-babel-evaluate nil)
+(defun pivotal-open-story-on-web ()
+  "Opens a Pivotal Story on the web when executed on a Story #"
+  (interactive)
+  ;(sample "45870133")
+  (let (link-name)
+    (setq link-name (thing-at-point 'word))
+    (shell-command (concat "open https://www.pivotaltracker.com/story/show/" link-name))))
+     ;(shell-command (concat "open x-devonthink-item:" record-location))
+
+(global-set-key "\C-c\C-p" 'pivotal-open-story-on-web)
+;; Authenticate Pivotal API
+(load "~/.emacs_auth")
 (provide 'custom)
 ;;;END CUSTOM
