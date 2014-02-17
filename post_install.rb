5455c393752dbfb8cd2f3eca08330cd2a3379680
Zander Hill <Zander@civet.ws>
2014-02-16 20:21:19 -0500

    Adding new sack
    Former-commit-id: a9972ab16d69b5e3b34b857d02de3abe00e5c80c

--- post_install.rb
+++ post_install.rb
@@ -14,10 +14,20 @@ Dir.chdir(File.expand_path("~/dotfiles")) do |dir|
   system "git submodule update"
 end
 
+def ensure_homesick_is_installed
+  require 'open3'
+  _, status = Open3.capture2('which homesick')
+  unless status.exitstatus == 0
+    `gem install homesick`
+  end
+end
+
+ensure_homesick_is_installed
+
 system "homesick symlink ~/dotfiles"
 
-%w[sack sag F].each do |file|
- remove_symlinks_and_relink(file, "~/bin_repos/sack/", "~/bin/")
+%w[sack F].each do |file|
+ remove_symlinks_and_relink(file, "~/bin_repos/sack/bin/", "~/bin/")
 end
 
 remove_symlinks_and_relink("sack.zsh", "~/bin_repos/sack/", "~/.zsh.d/")
@@ -25,6 +35,10 @@ remove_symlinks_and_relink("sack.zsh", "~/bin_repos/sack/", "~/.zsh.d/")
 # recompile YouCompleteMe
 you_complete_me_dir = Dir["home/.vim/bundle/youcompleteme"].first
 Dir.chdir(File.expand_path you_complete_me_dir) do |dir|
- system "./install.sh"
+  unless File.exists?('python/ycm_core.so')
+    system "./install.sh"
+  else
+    puts "YouCompleteMe already installed"
+  end
 end
 
