0f127f20c28d3b7793ce2804d1ad49f85296aad3
Zander Hill <Zander@civet.ws>
2014-02-24 21:46:23 -0500

    Add copy/paste functions inside tmux

--- home/.zsh.d/osx.zsh
+++ home/.zsh.d/osx.zsh
@@ -1,12 +1,5 @@
-# Mac OS X
+# Mac OS 
 if [[ "$OSTYPE" == darwin* ]]; then
-  alias o='open'
-  alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
-  alias slimit='pbpaste| html2slim| pbcopy'
-  export PATH=$HOME/Library/Python/2.7/bin:$PATH
-else
-  alias o='xdg-open'
-  alias get='wget --continue --progress=bar --timestamping'
 fi
 
 if [[ "$OSTYPE" == darwin* ]]; then
@@ -22,16 +15,41 @@ if [[ "$OSTYPE" == darwin* ]]; then
   alias pbc='pbcopy'
   alias pbp='pbpaste'
 
+  function pbcopy(){
+    if [ "$TERM" = "screen" -a -n "$TMUX" ]; then
+      reattach-to-user-namespace pbcopy
+    else
+      pbcopy
+    fi
+  }
+
+  function pbpaste(){
+
+    if [ "$TERM" = "screen" -a -n "$TMUX" ]; then
+      reattach-to-user-namespace pbpaste
+    else
+      pbpaste
+    fi
+  }
+
   alias trash_empty='rm -rf ~/.Trash/*'
 
   alias dsstore_rm='sudo find / -name ".DS_Store" -depth -exec rm {} \;'
+
   function system_urls(){
-    /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump |
-    egrep "(bindings.*\:$)" | sort
+    /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | \
+      egrep "(bindings.*\:$)" | sort
 }
 
   function dash() {
     open dash://$@
 }
 
+  alias o='open'
+  alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
+  alias slimit='pbpaste| html2slim | pbcopy'
+  export PATH=$HOME/Library/Python/2.7/bin:$PATH
+else
+  alias o='xdg-open'
+  alias get='wget --continue --progress=bar --timestamping'
 fi
