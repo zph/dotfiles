d8bc66e12addf76a2cfe8c07957197f2b3b8ae66
Zander Hill <Zander@civet.ws>
2014-04-02 21:49:34 -0400

    Add neato git add patch
    Former-commit-id: dc5b59406a827c2f4222ce0d8a92778151910580

--- home/.zsh.d/git.zsh
+++ home/.zsh.d/git.zsh
@@ -63,7 +63,13 @@ alias ga='git add'
 #compdef _git ga=git-add
 alias gaa='git add --all'
 #compdef _git ga=git-add
-alias gap='git add -p'
+if [[ -x $(which ffind) ]];then
+  function gap(){
+  git add --patch "$(ffind $1)"
+}
+else
+  alias gap='git add -p'
+fi
 #compdef _git ga=git-add
 alias gm='git merge'
 #compdef _git gm=git-merge
