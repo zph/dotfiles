156de1a241b8a61c8bc1cc870a81b90fc3957802
Zander Hill <Zander@civet.ws>
2014-06-24 13:47:19 -0400

    Improve expand_history.rb
    Former-commit-id: 80886a00e0c69cc7f0f9c6d439b9cdb9aeaeb556

--- home/bin/expand_history.rb
+++ home/bin/expand_history.rb
@@ -9,30 +9,44 @@ end
 
 # @zsh_history = %x(cat ~/.zsh_history |sed 's/\xc2\x91\|\xc2\x92\|\xc2\xa0\|\xe2\x80\x8e//' 2> /dev/null).split("\n")
 # opts = {invalid: :replace, undef: :replace, replace: '', universal_newline: true}
-@zsh_history = %x(cat ~/.zsh_history).remove_non_ascii.split("\n")
-@zsh_aliases = %x(zsh -i -c 'alias').remove_non_ascii.split("\n")
-@git_aliases = %x(git config --get-regexp alias*).remove_non_ascii.split("\n")
 
+def set_history_aliases(shell)
+  [
+    %x(cat ~/.#{shell}_history).remove_non_ascii.split("\n"),
+    %x(#{shell} -i -c 'alias').remove_non_ascii.split("\n"),
+    %x(git config --get-regexp alias*).remove_non_ascii.split("\n"),
+  ]
+end
 
-ZSH_ALIASES = {}
-@zsh_aliases.map do |a|
+case ENV['SHELL']
+when /zsh/i
+  shell_history, shell_aliases, git_aliases = set_history_aliases('zsh')
+when /bash/i
+  shell_history, shell_aliases, git_aliases = set_history_aliases('bash')
+else
+  warn "Unknown shell"
+  exit(1)
+end
+
+SHELL_ALIASES = {}
+shell_aliases.map do |a|
   begin
     array = a.split("=", 2)
-    ZSH_ALIASES[array[0]] = array[1].gsub(/['"]?/, '')
+    SHELL_ALIASES[array[0]] = array[1].gsub(/['"]?/, '')
   rescue
   end
 end
 
 
 GIT_ALIASES = {}
-@git_aliases.map do |a|
+git_aliases.map do |a|
   array = a.split(" ", 2)
   GIT_ALIASES[array[0].gsub(/alias\./, '')] = array[1]
 end
 
-converted_history = @zsh_history.map do |line|
+converted_history = shell_history.map do |line|
                       line.split.map do |item|
-                        ZSH_ALIASES[item] || GIT_ALIASES[item] || item
+                        SHELL_ALIASES[item] || GIT_ALIASES[item] || item
                       end.join(" ")
                     end
 
