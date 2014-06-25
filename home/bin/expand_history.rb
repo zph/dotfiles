abefb3d096d788d7582d0113cc3ee9b61fd93755
Zander Hill <Zander@civet.ws>
2014-06-24 22:12:34 -0400

    Improved expand_history.rb accuracy
    Former-commit-id: b86be5b40b730df0bd1e4bbfc46f1f230a481d2a

--- home/bin/expand_history.rb
+++ home/bin/expand_history.rb
@@ -7,61 +7,135 @@ class String
   end
 end
 
-# @zsh_history = %x(cat ~/.zsh_history |sed 's/\xc2\x91\|\xc2\x92\|\xc2\xa0\|\xe2\x80\x8e//' 2> /dev/null).split("\n")
-# opts = {invalid: :replace, undef: :replace, replace: '', universal_newline: true}
-
-def set_history_aliases(shell)
-  [
-    %x(cat ~/.#{shell}_history).remove_non_ascii.split("\n"),
-    %x(#{shell} -i -c 'alias').remove_non_ascii.split("\n"),
-    %x(git config --get-regexp alias*).remove_non_ascii.split("\n"),
-  ]
-end
-
-case ENV['SHELL']
-when /zsh/i
-  shell_history, shell_aliases, git_aliases = set_history_aliases('zsh')
-when /bash/i
-  shell_history, shell_aliases, git_aliases = set_history_aliases('bash')
-else
-  warn "Unknown shell"
-  exit(1)
-end
-
-SHELL_ALIASES = {}
-shell_aliases.map do |a|
+def swallow_errors
   begin
-    array = a.split("=", 2)
-    SHELL_ALIASES[array[0]] = array[1].gsub(/['"]?/, '')
+    yield
   rescue
   end
 end
 
-
-GIT_ALIASES = {}
-git_aliases.map do |a|
-  array = a.split(" ", 2)
-  GIT_ALIASES[array[0].gsub(/alias\./, '')] = array[1]
+SHELL_ALIAS_EXTRACTOR = ->(a, obj) do
+  array = a.split("=", 2)
+  obj[array[0]] = array[1].gsub(/['"]?/, '')
 end
 
-converted_history = shell_history.map do |line|
-                      line.split.map do |item|
-                        SHELL_ALIASES[item] || GIT_ALIASES[item] || item
-                      end.join(" ")
-                    end
+GIT_ALIAS_EXTRACTOR   = ->(a, obj) do
+  array = a.split(" ", 2)
+  obj[array[0].gsub(/alias\./, '')] = array[1]
+end
 
-# converted_history.each { |l| puts l }
-
-history_count = Hash.new(0)
-
-converted_history.each { |item| history_count[item] += 1 }
-
-sorted_history = history_count.sort_by { |k,v| v }.reverse
-
-all_items = sorted_history.each do |arr|
-  begin
-    $stdout.puts "#{arr[1]}: #{arr[0]}"
-  rescue Errno::EPIPE
-    exit(74)
+def generic_alias_extractor(aliases, func)
+  aliases.each_with_object({}) do |a, obj|
+    swallow_errors do
+      func.call(a, obj)
+    end
   end
 end
+
+def extract_shell_aliases(aliases, func = SHELL_ALIAS_EXTRACTOR)
+  generic_alias_extractor(aliases, func)
+end
+
+def extract_git_aliases(aliases, func = GIT_ALIAS_EXTRACTOR)
+  generic_alias_extractor(aliases, func)
+end
+
+def remove_duplicates(a)
+  # HACK:
+  # TODO: clean up original implementation so method's unneccessary
+
+  arr = a.split
+
+  arr.map.with_index do |e, i|
+    case
+    when (i + 1) == arr.length # last element in array
+      e
+    when e == arr[i + 1]
+      nil
+    else
+      e
+    end
+  end
+end
+
+def expand_word(item, prior_match, sa, ga)
+  return item if prior_match == item
+  case
+  when sa[item]
+    cmd = sa[item]
+    arr = expand_line(cmd, item, sa, ga)
+    remove_duplicates(arr)
+  when ga[item]
+    ga[item]
+  else
+    item
+  end
+end
+
+def expand_line(line, prior_match, sa, ga)
+  line.split.map do |item|
+    expand_word(item, prior_match, sa, ga)
+  end.join(" ")
+end
+
+def expand_history(history, shell_aliases, git_aliases)
+  history.map do |line|
+    expand_line(line, false, shell_aliases, git_aliases)
+  end
+end
+
+def get_history_aliases!(shell)
+  [
+    %x(cat ~/.#{shell}_history),
+    %x(#{shell} -i -c 'alias'),
+    %x(git config --get-regexp alias*),
+  ].map { |i| i.remove_non_ascii.chomp.split("\n") }
+end
+
+def retrieve_aliases!(shell = ENV['SHELL'])
+  bin = case shell
+        when /zsh/i
+          'zsh'
+        when /bash/i
+          'bash'
+        else
+          warn "YOLO: Unknown shell"
+          shell.split("/").last
+          exit if shell.empty?
+        end
+
+  get_history_aliases!(bin)
+end
+
+def print_history!(history, io = STDOUT)
+  history.each do |arr|
+    begin
+      io.puts "#{arr[1]}: #{arr[0]}"
+    rescue Errno::EPIPE
+      exit(74)
+    end
+  end
+end
+
+def main
+  shell_history, shell_aliases, git_aliases = retrieve_aliases!(ENV['SHELL'])
+
+  shell_aliases = extract_shell_aliases(shell_aliases)
+
+  git_aliases = extract_git_aliases(git_aliases)
+
+  # If nesting aliases deeper than this, fix that issue :P
+  # TODO: fix incorrect double replaces,
+  # - ie double replacing 'grep' on each round through expansion
+  history = expand_history(shell_history, shell_aliases, git_aliases)
+
+  history_count = Hash.new(0)
+
+  history.each { |item| history_count[item] += 1 }
+
+  sorted_history = history_count.sort_by { |k,v| v }.reverse
+end
+
+history = main
+
+print_history!(history)
