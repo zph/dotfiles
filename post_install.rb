9306d02b2411c59b0dc087798f7203f2f7655054
Zander Hill <zander@xargs.io>
2014-11-01 22:32:34 -0400

    Use post_install.rb to link golang bins in place
    Former-commit-id: 3994406755dd83283e537b1b89991b7f716b1687

--- post_install.rb
+++ post_install.rb
@@ -1,20 +1,30 @@
 #!/usr/bin/env ruby
 
-# TODO:
-# - learn to install default gems || create standalones automatically
-#
 require 'fileutils'
+require 'pathname'
+
+UNAME = `uname -a`
+
+def is_osx?
+  !! UNAME[/Darwin/]
+end
+
+def is_linux?
+  !! UNAME[/Linux/]
+end
 
 def remove_symlinks_and_relink(file, source, dest)
-  FileUtils.rm_rf(File.expand_path("#{dest}#{file}"))
+  FileUtils.rm_rf(File.expand_path(File.join(dest, file)))
   puts "Removing dead symlinks"
-  FileUtils.ln_s(File.expand_path("#{source}#{file}"), File.expand_path("#{dest}"), :force => true)
+  FileUtils.ln_s(File.expand_path(File.join(source, file)), File.expand_path("#{dest}"), :force => true)
   puts "Creating symlink for #{dest}"
 end
 
-Dir.chdir(File.expand_path("~/dotfiles")) do |dir|
-  system "git submodule init"
-  system "git submodule update"
+def clone_submodules
+  Dir.chdir(File.expand_path("~/dotfiles")) do |dir|
+    system "git submodule init"
+    system "git submodule update"
+  end
 end
 
 def ensure_homesick_is_installed
@@ -25,31 +35,59 @@ def ensure_homesick_is_installed
   end
 end
 
-ensure_homesick_is_installed
+def os_specific_binaries(os)
+  files = Dir["os_specific_binaries/#{os}/*"]
+  files.map { |f| File.expand_path(f) }
+end
 
-system "homesick symlink ~/dotfiles"
+def link_file(file)
+  filename = file.split("/").last.split(".").first
+  pathname = Pathname.new(file)
+  remove_symlinks_and_relink(filename, pathname.dirname, File.join("~/bin", filename))
+  `chmod +x #{File.join("~/bin", filename)}`
+end
 
-if `uname`[/Darwin/]
-  remove_symlinks_and_relink("sack", "~/bin_repos/go-sack/dist/", "~/bin/sack")
-else
-  remove_symlinks_and_relink("sack.linux_amd64", "~/bin_repos/go-sack/dist/", "~/bin/sack")
+def install_os_specific_binaries
+  case
+  when is_osx?
+    os_specific_binaries("osx").each {|f| link_file(f) }
+  when is_linux?
+    os_specific_binaries("linux").each {|f| link_file(f) }
+  when is_bsd?
+    os_specific_binaries("bsd").each {|f| link_file(f) }
+    warn "Many golang bins don't supply BSD compiled variants."
+  end
 end
 
 def bring_in_vim_plugins
   `infect`
 end
 
-bring_in_vim_plugins
-#
-# recompile YouCompleteMe
-you_complete_me_dir = Dir["home/.vim/bundle/youcompleteme"].first
-Dir.chdir(File.expand_path you_complete_me_dir) do |dir|
+def you_complete_me_setup
+  you_complete_me_dir = Dir["home/.vim/bundle/youcompleteme"].first
+  return unless you_complete_me_dir
 
-  `git submodule update --init --recursive`
-  unless File.exists?('python/ycm_core.so')
-    system "./install.sh"
-  else
-    puts "YouCompleteMe already installed"
+  Dir.chdir(File.expand_path you_complete_me_dir) do |dir|
+
+    `git submodule update --init --recursive`
+    unless File.exists?('python/ycm_core.so')
+      system "./install.sh"
+    else
+      puts "YouCompleteMe already installed"
+    end
   end
 end
 
+def main
+  clone_submodules
+  ensure_homesick_is_installed
+
+  system "homesick symlink ~/dotfiles"
+
+  install_os_specific_binaries
+
+  bring_in_vim_plugins
+  you_complete_me_setup
+end
+
+main
