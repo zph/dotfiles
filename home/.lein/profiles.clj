d35282ce80396f53dc26bcfec7c8438d60ed8b72
Zander Hill <zander@xargs.io>
2016-01-19 13:21:47 -0800

    Update profiles.clj
    Former-commit-id: babca693b7a746014bb58f45c473d0e2d9b6dc09

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,21 +1,19 @@
 {:user
  {:global-vars {^:dynamic *print-length* 200}
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.7"]
-                 [spyscope "0.1.5"]
+  :plugins      [[cider/cider-nrepl "0.10.0"]
+                 [refactor-nrepl "2.0.0-SNAPSHOT"]]
+  :dependencies [[alembic "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/tools.namespace "0.2.7"]
-                 [org.clojars.gjahad/debug-repl "0.3.3"]
-                 [refactor-nrepl "1.1.0"]
                  [org.clojure/tools.trace "0.7.8"]
+                 [spyscope "0.1.5"]
                  [evalive "1.1.0"]
                  [envvar "1.1.0"]
                  [io.aviso/pretty "0.1.8"]
                  [im.chit/vinyasa "0.3.4"]]
   :injections
   [(require 'spyscope.core)
-   (use 'spyscope.repl)
    (require '[vinyasa.inject :as inject])
-   (require 'io.aviso.repl)
     (require '[evalive.core])
    (inject/in ;; the default injected namespace is `.`
     ;;[vinyasa.inject :refer [inject [in inject-in]]]
@@ -23,11 +21,6 @@
 
     ;; imports all functions in vinyasa.pull
     [vinyasa.pull :all]
-
-    ;; same as [cemerick.pomegranate
-    ;;           :refer [add-classpath get-classpath resources]]
-    ;;[cemerick.pomegranate add-classpath get-classpath resources]
-
 
     ;; inject into clojure.core
     clojure.core
@@ -38,5 +31,4 @@
     [clojure.tools.trace trace-vars]
     [clojure.repl doc source]
     [clojure.pprint pprint pp]
-    [alex-and-georges.debug-repl debug-repl]
     [clojure.java.shell sh])]}}
