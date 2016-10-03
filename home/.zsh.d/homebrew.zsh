eb40de54c3d307662a0310876e399e68bac7c140
Zander Hill <zander@xargs.io>
2016-10-02 23:55:39 -0700

    Update homebrew configs
    Former-commit-id: c14ce1e4853a6b827da0571f47329f6dcd6071ac

--- home/.zsh.d/homebrew.zsh
+++ home/.zsh.d/homebrew.zsh
@@ -1,2 +1,3 @@
 export HOMEBREW_NO_ANALYTICS=1
 # git config --file="$(brew --repository)/.git/config" --replace-all homebrew.analyticsdisabled true
+export HOMEBREW_CASK_OPTS="--caskroom=/opt/homebrew-cask/Caskroom"
