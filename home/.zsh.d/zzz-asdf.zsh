f9c6a60cf865a0aacea4c716da3607fb6d453802
Zander Hill <zander@xargs.io>
2018-11-18 23:58:36 +0000

    Fix asdf for brew or manual install

--- home/.zsh.d/zzz-asdf.zsh
+++ home/.zsh.d/zzz-asdf.zsh
@@ -1,7 +1,14 @@
 # Issue, does not move ASDF to front of $PATH
 # so /usr/local/bin/erl will be used despite the asdf shim
-source $HOME/.asdf/asdf.sh
-source $HOME/.asdf/completions/asdf.bash
 
-zph/prepend_to_path "$HOME/.asdf/bin"
-zph/prepend_to_path "$HOME/.asdf/shims"
+if [[ -d $HOME/.asdf ]];then
+  ASDF_DIR="$HOME/.asdf"
+elif [[ -d /usr/local/opt/asdf ]];then
+  ASDF_DIR="/usr/local/opt/asdf"
+fi
+
+source $ASDF_DIR/asdf.sh
+source $ASDF_DIR/completions/asdf.bash
+
+zph/prepend_to_path "$ASDF_DIR/bin"
+zph/prepend_to_path "$ASDF_DIR/shims"
