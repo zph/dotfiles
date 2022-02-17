f3314198d3a29f197758fdf14245055edaceaba0
Zander Hill <zander@xargs.io>
2022-02-16 20:11:54 -0800

    Use 1p for agent

--- home/.zsh.d/ssh.zsh
+++ home/.zsh.d/ssh.zsh
@@ -1,2 +1,6 @@
 #Forwarding X11 with speedier compression & encryption
 #ssh -X -C -c blowfish -i KEY_FILE_PATH -l root INSTANCE_PUBLIC_DNS
+# yubikey_sock="/usr/local/var/run/yubikey-agent.sock"
+
+# export SSH_AUTH_SOCK="$yubikey_sock"
+export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
