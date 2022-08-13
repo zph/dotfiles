c176eb10097d6506e772f4f83c7758ff6aa88486
Zander Hill <zander@xargs.io>
2022-08-12 18:37:01 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,6 +65,9 @@ install_osx_packages(){
   # )
 
   if [[ ! -x "$(command -v brew)" ]];then
+    # TODO: no longer works on non-interactive tty
+    # Perhaps I can fix with running bash interactive?
+    # Workaround, execute the brew install separately and then continue install
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
