d94db646926b00b563e0453d8d76af659e779535
Zander Hill <zander@xargs.io>
2024-04-14 21:43:28 -0500

    Fix ubi script

--- home/.chezmoiscripts/run_onchange_after_070-install-ubi.sh
+++ home/.chezmoiscripts/run_onchange_after_070-install-ubi.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -eou pipefail
+set -x
 
 curl --silent --location \
     https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
@@ -10,10 +11,13 @@ mv ~/bin/ubi ~/bin_local/
 
 export PATH=~/bin_local:$PATH
 
-declare -a BINARIES=(
-  therootcompany/pathman
-)
+declare -a BINARIES=()
 
 for bin in "${BINARIES[@]}"; do
   ubi -p "$bin" --in ~/bin_local
 done
+
+# Special case
+ubi -p therootcompany/pathman -d -i ~/bin_local --tag v0.6.0 --exe pathman-v0.6.0-darwin-arm64
+# Workaround because of naming of binary inside tgz
+mv ~/bin_local/pathman-v0.6.0-darwin-arm64 ~/bin_local/pathman
