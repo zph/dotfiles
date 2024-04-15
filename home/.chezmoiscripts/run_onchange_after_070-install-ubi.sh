3474fe76945b85e6a2578b8b204da597a6751994
Zander Hill <zander@xargs.io>
2024-04-14 21:25:32 -0500

    Add ubi installer with registry of what to install

---
+++ home/.chezmoiscripts/run_onchange_after_070-install-ubi.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+curl --silent --location \
+    https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
+    sh
+
+mv ~/bin/ubi ~/bin_local/
+
+export PATH=~/bin_local:$PATH
+
+declare -a BINARIES=(
+  therootcompany/pathman
+)
+
+for
+for bin in "${BINARIES[@]}"; do
+  ubi -p "$bin" --in ~/bin_local
+done
