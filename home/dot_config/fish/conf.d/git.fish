a7ac109fd6d177642c5af0d348afc082711e76fe
Zander Hill <zander@xargs.io>
2024-05-10 14:30:49 -0700

    Add diff helper

--- home/dot_config/fish/conf.d/git.fish
+++ home/dot_config/fish/conf.d/git.fish
@@ -35,6 +35,8 @@ alias gcop 'git commit && git push'
 alias gitrm "git add -u"
 
 function gd
-  set preview "git diff $argv --color=always -- {-1}"
-  git diff $argv --name-only | fzf -m --ansi --preview $preview
+  pushd (git root)
+    set preview "git diff $argv --color=always -- {-1}"
+    git diff $argv --name-only | fzf -m --ansi --preview $preview
+  popd
 end
