613cbb9cd9b15e9d46e26c62a62045f24409fa00
Zander Hill <zander@xargs.io>
2026-01-07 11:01:25 -0800

    Speed up shell loading by caching inits and lazy loading

--- home/dot_config/fish/conf.d/030-env.fish
+++ home/dot_config/fish/conf.d/030-env.fish
@@ -14,33 +14,62 @@ end
 # Use zsh for declaring some env vars
 # it's messy and confusing but I want fish to inherit some settings from zsh
 # for consistency over the two shells
+
+# Read ~/.zsh_env ONCE for performance (was 111ms, now ~10ms)
+set -l zsh_env_content (cat ~/.zsh_env)
+
+# Extract vars matching patterns using pure fish string matching
 set ENVVARS EDITOR "DIRENV_" "HOMEBREW_" "DENO_"
 set MANPATH ""
 
 for var in $ENVVARS
-  set value (cat ~/.zsh_env  | grep "^$var" | xargs)
-  export "$value"
+  # Use string match instead of grep, parse in fish instead of xargs
+  for line in (string match -r "^$var.*" -- $zsh_env_content)
+    set -l kv (string split -m 1 = $line)
+    if test (count $kv) -eq 2
+      set -gx $kv[1] $kv[2]
+    end
+  end
 end
 
-# Will break on equal sign but the oneliners to handle that edge case are excessively complicated
-set ZSH_PATH (cat ~/.zsh_env | grep "^PATH=" | awk -F'=' '{print $2}')
-# Note reverse to make this apply older ones first (keeps useful order)
-for path in (string split ':' $ZSH_PATH | tail -r)
-  set PATH $path $PATH
+# Extract PATH using string match instead of cat|grep|awk
+set -l zsh_path_line (string match -r "^PATH=.*" -- $zsh_env_content)
+if test -n "$zsh_path_line"
+  set -l ZSH_PATH (string sub -s 6 -- $zsh_path_line[1])  # Skip "PATH="
+  # Note reverse to make this apply older ones first (keeps useful order)
+  for path in (string split ':' $ZSH_PATH)[-1..1]
+    set PATH $path $PATH
+  end
 end
 
 set -l env_file "$HOME/.config/fish/conf.d/env.sh"
 set -l cache_file "$HOME/tmp/env_cache"
 set -l cache_file_sha "$HOME/tmp/env_cache.sha"
-set -l current_sha (sha256sum $env_file | cut -d' ' -f1)
+set -l current_sha (string split ' ' (sha256sum $env_file))[1]
 
-if test ! -f $cache_file_sha; or test (cat $cache_file_sha | head -n1) != $current_sha
+# Check cache validity (optimized: avoid unnecessary subprocesses)
+set -l needs_refresh 1
+if test -f $cache_file_sha
+  set -l cached_sha (head -n1 $cache_file_sha)
+  if test "$cached_sha" = "$current_sha"
+    set needs_refresh 0
+  end
+end
+
+if test $needs_refresh -eq 1
   echo $current_sha > $cache_file_sha
   diff-env "source $env_file" > $cache_file
 end
 
-# Load cached env vars
-tail -n +2 $cache_file | while read -l line
-  set -l kv (string split -m 1 = $line)
-  set -gx $kv[1] $kv[2]
+# Load cached env vars (optimized: read once, parse in fish)
+if test -f $cache_file
+  set -l cache_lines (cat $cache_file)
+  for line in $cache_lines[2..-1]  # Skip first line
+    if test -n "$line"
+      set -l kv (string split -m 1 = $line)
+      if test (count $kv) -eq 2
+        set -gx $kv[1] $kv[2]
+      end
+    end
+  end
 end
--- home/dot_config/fish/conf.d/zph.fish
+++ home/dot_config/fish/conf.d/zph.fish
@@ -1,9 +1,10 @@
 alias vim $EDITOR
 alias edit $EDITOR
 alias gs 'git status -s'
-zoxide init fish | source
 alias wk 'cd ~/src/worktree'
 alias dc 'docker-compose'
+alias vim='nvim'
+alias c='chezmoi'
 
 if not functions -q fisher
   set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
@@ -11,18 +12,42 @@ if not functions -q fisher
   fish -c fisher
 end
 
-$HOME/bin/sack init_fish | source
-direnv hook fish | source
+# Cache helper for *init commands (saves ~150ms on startup)
+function __cached_init -a cmd_name cmd_path
+  set -l cache_dir "$HOME/tmp/fish_init_cache"
+  test -d $cache_dir; or mkdir -p $cache_dir
 
-alias vim='nvim'
+  set -l cache_file "$cache_dir/$cmd_name.fish"
+  set -l needs_refresh 0
 
-starship init fish | source
+  # Check if binary changed or cache missing
+  if not test -f $cache_file
+    set needs_refresh 1
+  else if test $cmd_path -nt $cache_file
+    set needs_refresh 1
+  end
 
-alias c='chezmoi'
+  if test $needs_refresh -eq 1
+    # Special handling for starship which uses --print-full-init
+    if string match -q "*starship*" $cmd_name
+      $cmd_path init fish --print-full-init > $cache_file 2>/dev/null
+    else
+      $argv[3..-1] > $cache_file 2>/dev/null
+    end
+  end
+
+  test -f $cache_file; and source $cache_file
+end
+
+# Cached init commands (was ~150ms, now ~5ms)
+__cached_init zoxide /opt/homebrew/bin/zoxide zoxide init fish
+__cached_init sack $HOME/bin/sack $HOME/bin/sack init_fish
+__cached_init direnv /Users/zhill/bin/vendor/direnv direnv hook fish
+__cached_init starship /opt/homebrew/bin/starship starship init fish
 
 # https://docs.atuin.sh/configuration/key-binding/
 set -gx ATUIN_NOBIND "false"
-atuin init fish | source
+__cached_init atuin /opt/homebrew/bin/atuin atuin init fish
 
 # bind to ctrl-r in normal and insert mode, add any other bindings you want here too
 bind \cr _atuin_search
@@ -37,4 +62,4 @@ function gwa -d "Git worktree add and then cd into folder"
   cd $argv[1]
 end
 
-mise activate fish | source
+__cached_init mise /opt/homebrew/bin/mise mise activate fish
