31f2c72eff4e0894710177af1b2d5d74ec2445cd
Zander Hill <zander@xargs.io>
2026-03-15 08:31:25 -0700

    Fix omf install failure during chezmoi apply
    Remove legacy ZZZ.fish (superseded by ZZZ-path.fish) which called
    bash without a guard, breaking PATH on fresh installs. Fix omf.fish
    lazy-loader to explicitly source the real omf function instead of
    using `functions -e`, which triggers a fish autoload cache bug.

--- home/dot_config/fish/conf.d/omf.fish
+++ home/dot_config/fish/conf.d/omf.fish
@@ -6,11 +6,11 @@ set -q XDG_DATA_HOME
 # Lazy-load Oh My Fish - only initialize when omf command is used
 # This significantly improves startup time
 function omf --description "Oh My Fish - lazy loaded"
-  # Remove this function to avoid recursion
-  functions -e omf
-
-  # Load Oh My Fish configuration
+  # Load Oh My Fish configuration (this defines the real omf function,
+  # replacing this wrapper). We avoid `functions -e omf` because fish
+  # caches the erasure and won't autoload the real omf afterward.
   source $OMF_PATH/init.fish
+  source $OMF_PATH/pkg/omf/functions/omf.fish
 
   # Call omf with original arguments
   omf $argv
